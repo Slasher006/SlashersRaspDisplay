67f00bcac88f482c6b64d368b70173b39384ccff
Slasher006 <martinwietzke72@gmail.com>
2015-07-30 10:42:12 +0200

    miminale locale aenderung

--- compile.sh
+++ compile.sh
@@ -4,8 +4,8 @@
 
 gcc -o slashdispshow pcd8544_rpi.cpp PCD8544.cpp -L/usr/lib -lwiringPi
 
-#sudo systemctl stop LCDslash.service
+sudo systemctl stop LCDslash.service
 
-#cp slashdispshow /home/slasher006/slashdispshow
+cp slashdispshow /home/slasher006/slashdispshow
 
-#sudo systemctl restart LCDslash.service
+sudo systemctl restart LCDslash.service
